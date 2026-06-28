7006c7f08def8a7f4945c6eef2426ccc28ffc1ce
DaniDanSR <carrerodanilo07@gmail.com>
2026-06-28 10:20:26 -0600

    Agregar nuevos dto

---
+++ API/API/DTO/AgregarFunciones.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTO
+{
+    public class AsignarFuncionesDTO
+    {
+        [Required] public int IdRol { get; set; }
+        [Required] public List<int> IdFunciones { get; set; } = new List<int>();
+    }
+}
